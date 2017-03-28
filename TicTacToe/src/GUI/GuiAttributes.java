6d947e4e6b46f7fe993a79c8fc37560f3f886aa8
qwerty12794 <onzin09@gmail.com>
2017-03-28 10:13:12 +0200

    Gui class is edited, a grid is made and we can detect where the mouse is clicked. Made a half function so that we can draw a circle or cross in a box

---
+++ TicTacToe/src/GUI/GuiAttributes.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+
+public class GuiAttributes {
+    GraphicsContext graphicsContext;
+    Label label;
+
+    public GuiAttributes(GraphicsContext graphicsContext){
+        try{
+            this.graphicsContext = graphicsContext;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
