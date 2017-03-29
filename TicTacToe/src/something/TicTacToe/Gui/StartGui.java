b6f03f86d5a36955a38933fe0978eaf98bddbef6
samikroon <samikroon@gmail.com>
2017-03-29 14:38:47 +0200

    Add waitPopUP and confirmDialog

--- TicTacToe/src/something/TicTacToe/Gui/StartGui.java
+++ TicTacToe/src/something/TicTacToe/Gui/StartGui.java
@@ -2,14 +2,20 @@ package something.TicTacToe.Gui;
 
 
 import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import something.TicTacToe.Controller;
+
+import java.util.Optional;
 
 
 public class StartGui extends Application{
     private Stage primaryStage;
     private Stage gameStage;
     private Controller controller;
+    private Alert waitAlert, confirmGame;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -20,7 +26,6 @@ public class StartGui extends Application{
         this.primaryStage.setTitle("menu");
         this.primaryStage.setScene(new InitPopUp(controller).scene);
         this.primaryStage.show();
-
 
     }
 
@@ -37,6 +42,36 @@ public class StartGui extends Application{
         gameStage.close();
         primaryStage.show();
     }
+
+    public void waitPopUp () {
+        waitAlert = new Alert(Alert.AlertType.INFORMATION);
+        waitAlert.setTitle("Tic Tac Toe");
+        waitAlert.setHeaderText(null);
+        waitAlert.setContentText("Waiting for a game");
+        waitAlert.initStyle(StageStyle.UNDECORATED);
+        waitAlert.getDialogPane().lookupButton(ButtonType.OK).setDisable(true);
+        waitAlert.show();
+    }
+
+    public void closeWaitPopUp() {
+        waitAlert.close();
+    }
+
+    public boolean confirmGameDialog(String opponentName) {
+        confirmGame = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmGame.setTitle("You have been challenged");
+        confirmGame.setHeaderText("Challenger: " + opponentName);
+        confirmGame.setContentText("Would you like to accept this game?");
+
+        Optional<ButtonType> result = confirmGame.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
     public static void main(String[] args){ Application.launch(args); }
 
